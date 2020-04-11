48dfd2ab81071d90cf8b9c3254dfc2d95b25e2a0
giray <13612885+GirayEryilmaz@users.noreply.github.com>
2020-04-11 14:36:34 +0300

    performance improvement

--- g-be-gone.js
+++ g-be-gone.js
@@ -2,19 +2,42 @@
 // @name     g be gone
 // @version  1
 // @grant    none
-// @match        https://www.google.com/search*
+// @match		 https://www.google.com/search*
 // ==/UserScript==
 
 
 
+//alert("g be gone");
+
+
+
+
+function remove_this(arr, str){
+    for (var i = 0; i < arr.length; i++) {
+      arr[i].removeAttribute(str);
+
+      var jsac =  arr[i].getAttribute('jsaction');
+      if (jsac === null){
+        // console.log('hi null'); // for debugging
+      }else{
+        
+        var start = jsac.indexOf(str+ ':');
+        var colon = start + (str+':').length;
+        var end = jsac.indexOf(';', colon);
+        var fullstr = jsac.substring(start,end+1);
+        var newjsac = jsac.replace(fullstr, '');
+        arr[i].setAttribute('jsaction', newjsac);
+      }
+
+
+		}
+}
+
 
 
 var anchors = document.getElementsByTagName("a");
+remove_this(anchors, 'onmousedown');
+remove_this(anchors, 'mousedown');
 
-//alert("g be gone");
 
-for (var i = 0; i < anchors.length; i++) {
-    anchors[i].onmousedown = null;         // needed for all
-    anchors[i].removeAttribute('jsaction'); // needed for images
 
-}
